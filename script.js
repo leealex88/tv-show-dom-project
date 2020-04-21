e6cd5e291db43bc1f8a8832bdd421ea3d19ec0b7
ola <olasvoice@gmail.com>
2020-04-21 23:50:56 +0100

    push
    .

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
+  const displayAllEpisods = allEpisodes.map((episod) => {
+    return episod.id;
+  });
+  return;
 }
 
 function makePageForEpisodes(episodeList) {
@@ -10,3 +14,9 @@ function makePageForEpisodes(episodeList) {
 }
 
 window.onload = setup;
+
+let myElement = document.querySelector("#root");
+let paragraph = document.createElement("p");
+myElement.appendChild(paragraph);
+console.log(paragraph);
+paragraph.innerText = setup();
