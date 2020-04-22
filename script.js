4b8f0f5035127dd16acf02dcb8d130f8328ffc37
ola <olasvoice@gmail.com>
2020-04-22 13:51:38 +0100

    first part
    still working on

--- script.js
+++ script.js
@@ -1,22 +1,58 @@
 //You can edit ALL of the code here
+const rootElem = document.getElementById("root");
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
-  const displayAllEpisods = allEpisodes.map((episod) => {
-    return episod.id;
+  episodeCode(allEpisodes);
+}
+function episodeCode(allEpisodes) {
+  const createAcode = allEpisodes.map((episode) => {
+    episode.number;
   });
-  return;
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  const movieLiest = episodeList.map((episod) => {
+    // console.log(episod);
+
+    //single card
+    const divCard = document.createElement("div");
+    divCard.classList.add("single-card");
+    rootElem.appendChild(divCard);
+    // h3 - titel tag / the episod number to be added
+    const pTag = document.createElement("p");
+    pTag.classList.add("titel");
+    divCard.appendChild(pTag);
+    pTag.innerHTML = episod.name;
+    // image div
+    const imageDiv = document.createElement("img");
+    imageDiv.classList.add("image");
+    divCard.appendChild(imageDiv);
+    imageDiv.src = episod.image.medium;
+    // description
+    const pSummary = document.createElement("p");
+    pSummary.classList.add("summary");
+    divCard.appendChild(pSummary);
+    pSummary.innerHTML = episod.summary;
+  });
 }
 
 window.onload = setup;
 
-let myElement = document.querySelector("#root");
-let paragraph = document.createElement("p");
-myElement.appendChild(paragraph);
-console.log(paragraph);
-paragraph.innerText = setup();
+// const displayEpisod = allEpisodes.map((episod) => {
+//
+//   let paragraph = document.createElement("p");
+//   card.appendChild(paragraph);
+//   paragraph.innerHTML = episod.name;
+//   doc.appendChild(paragraph);
+//   let paragraph1 = document.createElement("p");
+//   card.appendChild(paragraph1);
+//   paragraph1.innerHTML = `the season number ${episod.season}`;
+//   doc.appendChild(paragraph1);
+//   let paragraph3 = document.createElement("p");
+//   card.appendChild(paragraph3);
+//   paragraph3.innerHTML = `the episode number ${episod.number}`;
+//   doc.appendChild(paragraph3);
+// });
+// console.log(allEpisodes);
